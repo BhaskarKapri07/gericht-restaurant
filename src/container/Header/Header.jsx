118dcfa91137c2071fc39cde0dff325f3184e09b
Bhaskar Kapri <kapri.b@somaiya.edu>
2023-11-15 05:13:28 +0530

    add and style header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import images from "../../constants/images";
+import "./Header.css";
+import { SubHeading } from "../../components";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin:'2rem 0'}}>A taste of luxury. Celebrate the confluence of fine food and artistic flair. Savor the art of exquisite flavors in a realm where culinary excellence is the standard.</p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
